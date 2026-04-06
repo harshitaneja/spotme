fb7bb81bb230b3afa2422f4d93548937be35ee5b
Harshit Aneja <harshit@live.in>
2026-04-06 21:45:33 +0530

    chore: cleanup scratch binaries and unnecessary source files

--- test_picker.rs
+++
@@ -1,6 +0,0 @@
-use ratatui_image::picker::Picker;
-
-fn main() {
-    let mut picker = Picker::from_query_stdio().unwrap();
-    println!("Protocol guessed: {:?}", picker.protocol_type());
-}
--- test_variants.rs
+++
@@ -1,8 +0,0 @@
-use ratatui_image::picker::ProtocolType;
-
-fn main() {
-    let _ = ProtocolType::Halfblocks;
-    let _ = ProtocolType::Sixel;
-    let _ = ProtocolType::Kitty;
-    let _ = ProtocolType::Iterm2;
-}
